073dbc6c2364aa2550110879299e7b0d4216f871
hatembkh <boukhroufhatem04@gmail.com>
2025-04-18 12:58:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import  { useState } from 'react';
 import './App.css';
-
+import selfie from './slefie.jpg';
 function App() {
+  const [Person] = useState({
+    fullName: "Hatem Boukhrouf",
+    bio: "I am a beginner in React",
+    profession: "Web Developer",
+    imgSrc: {selfie},
+  });
+  const [show, setShow] = useState(false);
+  function shows(){ 
+      return setShow(!show)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={shows}>Show</button>
+      {show && <div>
+          <h1>{Person.fullName}</h1>
+          <h2>{Person.bio}</h2>
+          <h3>{Person.profession}</h3>
+          <img src={selfie} alt="selfie" />
+        </div>}
     </div>
   );
 }
